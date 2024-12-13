95ff3957486dd185860eb2e865fc7e5cac3d7092
04nhatminh <04nhatminh@gmail.com>
2024-12-13 08:59:03 +0700

    NMinh: Update search users in search page

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -1,11 +1,52 @@
 let controller = {};
+const models = require('../models');
+const { Op } = require('sequelize');
 
 controller.renderSearch = async (req, res) => {
+    const userId = isNaN(req.cookies.userId) ? null : parseInt(req.cookies.userId);
+    const currentUser = await models.User.findOne({ where: { id: userId } });
+
+    res.locals.currentUser = currentUser;
+
     res.render('search', {
         title: "Search • Simple Threads",
         isSearch: true,
-        loggedIn: true,
+        loggedIn: currentUser ? true : false,
     });
+}
+
+controller.loadUsers = async (req, res) => {
+    const userId = isNaN(req.cookies.userId) ? null : parseInt(req.cookies.userId);
+    const keywords = req.query.keywords;
+
+    let options = { where: {} };
+
+    if (keywords.trim() != "") {
+        options.where[Op.or] = {
+            username: { [Op.iLike]: `%${keywords.trim()}%` },
+            description: { [Op.iLike]: `%${keywords.trim()}%` },
+        }
+    }
+
+    try {
+        const users = await models.User.findAll({
+            where: {
+                id: { [Op.ne]: userId },
+                ...options.where,
+            },
+            include: ['followers'],
+        });
+
+        res.json(users.map(user => ({
+            username: user.username,
+            avatarUrl: user.avatarUrl,
+            description: user.description || '',
+            followers: user.followers.length || [],
+        })));
+    } catch (error) {
+        console.error('Error when load users:', error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }
 }
 
 module.exports = controller;
--- src/routes/search.js
+++ src/routes/search.js
@@ -3,5 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', searchController.renderSearch);
+router.get('/loadUsers', searchController.loadUsers);
 
 module.exports = router;
